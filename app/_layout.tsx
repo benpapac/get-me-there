b8c99b1022f9fb328feceae5aeee1f97f62787fd
benpapac <ben.papac@gmail.com>
2025-05-18 14:50:54 -0400

    auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,13 +13,15 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  const []
+
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <GoogleOAuthProvider clientId="<your_client_id>">
+    <GoogleOAuthProvider clientId={`${process.env.EXPO_PUBLIC_CLIENT}`}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
